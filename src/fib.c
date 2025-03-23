23c4263ea1850ae0c1f854b7e2711664dc40abfd
pyrod3v <192714658+pyrod3v@users.noreply.github.com>
2025-03-23 16:04:24 +0200

    fix: add missing newline in error text

--- src/fib.c
+++ src/fib.c
@@ -19,7 +19,7 @@ limitations under the License.
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <number>", argv[0]);
+        fprintf(stderr, "Usage: %s <number>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
