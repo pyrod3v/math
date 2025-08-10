b5f7d87bfa7c7d7e8b70e21f0f2547491bdbbd93
pyrod3v <192714658+pyrod3v@users.noreply.github.com>
2025-08-10 16:27:07 +0300

    Add SIMD variant of find_squares

--- sqr/sqr.c
+++ sqr/sqr.c
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#include <immintrin.h>
 #include <stdio.h>
 #include <time.h>
 
@@ -37,6 +38,23 @@ void find_squares(const int i) {
 
         // prepare n for next step
         n += 2;
+    }
+}
+
+void find_squares_simd(int32_t *nums, size_t n) {
+    const size_t lanes = 8; // AVX2 is 256-bit / 32-bit -> 8 lanes
+    const __m256i inc = _mm256_setr_epi32(0,1,2,3,4,5,6,7);
+
+    for (size_t i = 0; i + lanes <= n; i += lanes) {
+        __m256i base = _mm256_set1_epi32((int32_t)i + 2);
+        __m256i n = _mm256_add_epi32(base, inc); // i+2, i+3, etc
+        __m256i x  = _mm256_mullo_epi32(n, n);
+        _mm256_storeu_si256((__m256i*)(nums + i), x);
+    }
+
+    for (int i = 0; i < n; ++i) {
+        int32_t v = i + 2;
+        nums[i] = v * v;
     }
 }
 
